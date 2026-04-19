cf4fbeba8660fd70b00193f626a9c1f8ef3e5243
gunjourain112 <43371733+gunjourain112@users.noreply.github.com>
2026-04-19 16:37:06 +0900

    feat: implement auth service

--- gin/go.mod
+++ gin/go.mod
@@ -36,6 +36,7 @@ require (
 	github.com/go-playground/validator/v10 v10.30.2 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/goccy/go-yaml v1.19.2 // indirect
+	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/hashicorp/hcl/v2 v2.18.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
@@ -65,12 +66,12 @@ require (
 	go.uber.org/dig v1.19.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/arch v0.22.0 // indirect
-	golang.org/x/crypto v0.49.0 // indirect
-	golang.org/x/mod v0.33.0 // indirect
-	golang.org/x/net v0.51.0 // indirect
+	golang.org/x/crypto v0.50.0 // indirect
+	golang.org/x/mod v0.34.0 // indirect
+	golang.org/x/net v0.52.0 // indirect
 	golang.org/x/sync v0.20.0 // indirect
-	golang.org/x/sys v0.42.0 // indirect
-	golang.org/x/text v0.35.0 // indirect
+	golang.org/x/sys v0.43.0 // indirect
+	golang.org/x/text v0.36.0 // indirect
 	google.golang.org/protobuf v1.36.10 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ gin/internal/auth/service.go
@@ -0,0 +1,60 @@
+package auth
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/gunjourain112/cloud-we-go-server/gin/internal/config"
+	"github.com/gunjourain112/cloud-we-go-server/gin/internal/user"
+)
+
+type Service interface {
+	Register(ctx context.Context, req *RegisterRequest) error
+	Login(ctx context.Context, req *LoginRequest) (*TokenResponse, error)
+}
+
+type service struct {
+	userRepo user.Repository
+	cfg      *config.Config
+}
+
+func NewService(userRepo user.Repository, cfg *config.Config) Service {
+	return &service{userRepo: userRepo, cfg: cfg}
+}
+
+func (s *service) Register(ctx context.Context, req *RegisterRequest) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	_, err = s.userRepo.Create(ctx, req.Email, string(hash), req.Name)
+	return err
+}
+
+func (s *service) Login(ctx context.Context, req *LoginRequest) (*TokenResponse, error) {
+	u, err := s.userRepo.GetByEmail(ctx, req.Email)
+	if err != nil {
+		return nil, errors.New("invalid credentials")
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(u.PasswordHash), []byte(req.Password)); err != nil {
+		return nil, errors.New("invalid credentials")
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": u.ID.String(),
+		"exp": time.Now().Add(time.Duration(s.cfg.JWT.ExpireHours) * time.Hour).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte(s.cfg.JWT.Secret))
+	if err != nil {
+		return nil, err
+	}
+
+	return &TokenResponse{AccessToken: tokenString}, nil
+}
